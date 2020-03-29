951562ea07bd3a9b7ec6254acc02e90c25924dde
SagarMakhar <sagarmakhar@gmail.com>
2020-03-29 15:18:34 +0000

    fs: Import changes for async discard support
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: naonana <hecticnao@outlook.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1392,7 +1392,9 @@ errout:
 		if (inode->i_nlink)
 			ext4_orphan_del(NULL, inode);
 	}
-
+#if defined(CONFIG_PRODUCT_REALME_SDM710) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 	return ret ? ret : copied;
 }
 
@@ -1509,7 +1511,9 @@ errout:
 		if (inode->i_nlink)
 			ext4_orphan_del(NULL, inode);
 	}
-
+#if defined(CONFIG_PRODUCT_REALME_SDM710) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 	return ret ? ret : copied;
 }
 
@@ -4259,6 +4263,9 @@ void ext4_truncate(struct inode *inode)
 
 	if (inode->i_size & (inode->i_sb->s_blocksize - 1))
 		ext4_block_truncate_page(handle, mapping, inode->i_size);
+#if defined(CONFIG_PRODUCT_REALME_SDM710) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 
 	/*
 	 * We add the inode to the orphan list, so that if this
