2e5929c724dbeaa723342dc7be1f0b101b808114
Lau <laststandrighthere@gmail.com>
2021-07-10 14:25:33 +0000

    cpufreq: schedutil: tune rate limit from pixel
    Signed-off-by: Reinazhard <reinazhard@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -757,18 +757,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-#ifdef CONFIG_SCHED_WALT
-	/*
-	 * NOTE:
-	 * intializing up_rate/down_rate to 0 explicitly in kernel
-	 * since WALT expects so by default.
-	 */
-	tunables->up_rate_limit_us = 0;
-	tunables->down_rate_limit_us = 0;
-#else
-	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
-#endif
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 20000;
 	tunables->iowait_boost_enable = true;
 
 	policy->governor_data = sg_policy;
